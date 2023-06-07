6ceac930bff1289acbea26b6647ad5c2312f205f
oliveragabriel <g.olivera1993@gmail.com>
2023-06-07 20:45:09 -0300

    fix: adjust sql to update line at tables

--- back-end/user/service/sql/userQueries.js
+++ back-end/user/service/sql/userQueries.js
@@ -4,7 +4,7 @@ exports.getUserByIdQuery = 'SELECT * FROM usuario WHERE id = $1';
 
 exports.postAddNewUserQuery = 'INSERT INTO usuario( nome, sobrenome, atribuicao, senha) VALUES ( $1, $2, $3, $4 );';
 
-exports.putEditUserByIdQuery = 'ALTER TABLE usuario( nome, sobrenome, dt_nascimento, cpf, nacionalidade, senha ) VALUES( $1, $2, $3, $4, $5, $6 ) WHERE id_usuario = $7;'
+exports.putEditUserByIdQuery = 'UPDATE usuario SET nome = $1, sobr) VALUES( $1, $2, $3, $4, $5, $6 )enome = $2, dt_nascimento = $3, cpf = $4, nacionalidade = $5, senha = $6 WHERE id_usuario = $7;'
 
 exports.deleteUserByIdQuery = 'DELETE FROM usuario WHERE id = $1';
 
@@ -12,7 +12,7 @@ exports.getUserContactQuery = 'SELECT contato.ddd, contato.numero, contato.email
 
 exports.postAddNewContactQuery = 'INSERT INTO contato( ddd, numero, email, id_usuario) VALUES ( $1, $2, $3, $4 );';
 
-exports.putEditContactByIdQuery = 'ALTER TABLE usuario( ddd, numero, email ) VALUES( $1, $2, $3 ) WHERE id_usuario = $4;'
+exports.putEditContactByIdQuery = 'UPDATE contato SET ddd = $1, numero = $2, email = $3 WHERE id_usuario = $4;'
 
 exports.deleteContactByIdQuery = 'DELETE FROM contato WHERE id = $1';
 
@@ -20,6 +20,6 @@ exports.getUserAddressQuery = 'SELECT endereco.rua, endereco.numero, endereco.co
 
 exports.postAddNewAddressQuery = 'INSERT INTO endereco( rua, numero, complemento, bairro, cidade, id_estado, id_pais, id_usuario) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8 );'
 
-exports.putEditAddressByIdQuery = 'ALTER TABLE usuario( rua, numero, complemento, bairro, cidade, id_estado, id_pais) VALUES( $1, $2, $3, $4, $5, $6, $7  ) WHERE id_usuario = $8;'
+exports.putEditAddressByIdQuery = 'UPDATE endereco SET rua = $1, numero = $2, complemento = $3, bairro = $4, cidade = $5, id_estado = $6, id_pais = $7 WHERE id_usuario = $8;'
 
 exports.deleteAddressByIdQuery = 'DELETE FROM endereco WHERE id = $1';
