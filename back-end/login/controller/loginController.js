6312abf86d60afd5e3886e4116858295bbb9ee03
oliveragabriel <g.olivera1993@gmail.com>
2023-06-20 19:50:25 -0300

    fix: adjust imports from login route

--- back-end/login/controller/loginController.js
+++ back-end/login/controller/loginController.js
@@ -4,7 +4,7 @@ exports.validateUserToLogin = (req, res) => {
   const { idUsuario } = req.params
   const { senha } = req.body
   loginService.validateUserToLogin(idUsuario, senha)
-    .then((result) => {
+    .then(() => {
       res.status(200).send({ message: 'Login realizado com sucesso!' });
     })
     .catch((error) => {
